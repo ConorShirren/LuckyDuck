3350b463f2148fd57df9ae6742eebf19204a64b4
conorshirren <cshirren@gmail.com>
2020-07-28 16:30:05 +0100

    Building Client UI. Testing HTTP connection with dotnet APIs

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct} from './models/product';
+import { IPagination} from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'LuckyDuck Vintage';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe((response: IPagination) => {
+      this.products = response.data;
+    }, error => {
+      console.log('error', error);
+    });
+  }
 }
